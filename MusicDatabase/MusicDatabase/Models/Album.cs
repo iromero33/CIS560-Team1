1659245c1bf392adac4ee1438e9eddae455ec07b
Izabel Romero <iromero@ksu.edu>
2023-04-19 09:13:58 -0500

    Removed RecordLabel and Artist attribute streamingservice

--- MusicDatabase/MusicDatabase/Models/Album.cs
+++ MusicDatabase/MusicDatabase/Models/Album.cs
@@ -6,16 +6,13 @@ namespace MusicDatabase.Models
     {
         public int AlbumID { get; }
 
-        public int LabelID { get; }
-
         public string Name { get; }
 
         public DateTime ReleaseDate { get; }
 
-        internal Album(int albumId, int labelId, string name, DateTime releaseDate)
+        internal Album(int albumId, string name, DateTime releaseDate)
         {
             AlbumID = albumId;
-            LabelID = labelId;
             Name = name;
             ReleaseDate = releaseDate;
         }
