ab55a92a3da384defe2be8985fb37464364e3809
Joe DeWitt <jwdewitt@ksu.edu>
2023-04-19 09:57:45 -0500

    Added some procedures

---
+++ MusicDatabase/MusicDatabase/SQL/Procedures/Music.CreateSong.sql
@@ -0,0 +1,14 @@
+﻿CREATE OR ALTER PROCEDURE Music.CreateArtist
+   @Title NVARCHAR(64),
+   @ArtistID INT,
+   @AlbumID INT,
+   @GenreID INT,
+   @SpotifyID INT,
+   @SongID INT OUTPUT
+AS
+
+INSERT Music.Artist(Title, ArtistID, AlbumID, GenreID, SpotifyID, SongID)
+VALUES(@Title, @ArtistID, @AlbumID, @GenreID, @SpotifyID, @SongID);
+
+SET @SongID = SCOPE_IDENTITY();
+GO
---
+++ MusicDatabase/MusicDatabase/SQL/Procedures/Music.GetArtist.sql
@@ -0,0 +1,8 @@
+﻿CREATE OR ALTER PROCEDURE Music.GetArtist
+   @Name NVARCHAR(64)
+AS
+
+SELECT A.Name
+FROM Music.Artist A
+WHERE A.Name = @Name;
+GO
