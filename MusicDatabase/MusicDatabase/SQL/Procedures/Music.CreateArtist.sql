961911c7671ff1374a20653d9237cc89dab6dc93
Joe DeWitt <jwdewitt@ksu.edu>
2023-04-19 09:29:22 -0500

    Created procedures folder

---
+++ MusicDatabase/MusicDatabase/SQL/Procedures/Music.CreateArtist.sql
@@ -0,0 +1,10 @@
+﻿CREATE OR ALTER PROCEDURE Music.CreateArtist
+   @Name NVARCHAR(32),
+   @ArtistID INT OUTPUT
+AS
+
+INSERT Person.Person([Name])
+VALUES(@Name);
+
+SET @ArtistID = SCOPE_IDENTITY();
+GO
