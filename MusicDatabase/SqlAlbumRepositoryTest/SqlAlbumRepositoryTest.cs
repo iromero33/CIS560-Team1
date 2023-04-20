1d826f23bca2554df63f761f0e2485ca48b87ff2
Joe <jwdewitt@ksu.edu>
2023-04-20 14:34:32 -0500

    Added album test

---
+++ MusicDatabase/SqlAlbumRepositoryTest/SqlAlbumRepositoryTest.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Transactions;
+using MusicDatabase.Models;
+using MusicDatabase;
+
+namespace MusicDatabase.Tests
+{
+    [TestClass]
+    public class SqlAlbumRepositoryTest
+    {
+
+        const string connectionString = @"Server=(localdb)\MSSQLLocalDb;Database=   ;Integrated Security=SSPI;";
+
+        private static string GetTestString() => Guid.NewGuid().ToString("N");
+
+        private SqlAlbumRepository albumRepo;
+        //private SqlAddressRepository addressRepo;
+        private TransactionScope transaction;
+
+        [TestInitialize]
+        public void InitializeTest()
+        {
+            personRepo = new SqlPersonRepository(connectionString);
+            addressRepo = new SqlAddressRepository(connectionString);
+
+            transaction = new TransactionScope();
+        }
+
+        [TestCleanup]
+        public void CleanupTest()
+        {
+            transaction.Dispose();
+        }
+
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
