9614840bd13dad7f5e99c08f46dccf4a73e49ae9
Joe DeWitt <jwdewitt@ksu.edu>
2023-04-23 15:27:27 -0500

    Changed Music.GetSongByReleaseDate

--- MusicDatabase/MusicDatabaseNEW/Sql/Procedures/Music.GetSongByReleaseDate.sql
+++ MusicDatabase/MusicDatabaseNEW/Sql/Procedures/Music.GetSongByReleaseDate.sql
@@ -1,9 +1,9 @@
-CREATE OR ALTER PROCEDURE Music.GetSongByReleaseYear
-	@Year DATETIMEOFFSET
+CREATE OR ALTER PROCEDURE Music.GetSongByReleaseDate
+	@ReleaseDate DATETIMEOFFSET
 AS
 
 SELECT S.SongID, S.Title, S.ArtistID, S.AlbumID, S.GenreID, S.SpotifyListens
 FROM Music.Song S
   INNER JOIN Music.Album A ON A.AlbumID = S.AlbumID
-WHERE YEAR(A.ReleaseDate) = YEAR(@Year);
+WHERE A.ReleaseDate = @ReleaseDate
 GO
