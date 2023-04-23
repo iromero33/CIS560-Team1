7630dff08716181b30bcaadff866f33754cfba0c
Joe DeWitt <jwdewitt@ksu.edu>
2023-04-23 15:57:48 -0500

    Added GetSongsBySpotifyListensDESC and GetSongsBySpotifyListensASC procedures

---
+++ MusicDatabase/MusicDatabaseNEW/Sql/Procedures/Music.GetSongsBySpotifyListensASC.sql
@@ -0,0 +1,7 @@
+CREATE OR ALTER PROCEDURE Music.GetSongsBySpotifyListensASC
+AS
+
+SELECT S.SongID, S.Title, S.ArtistID, S.AlbumID, S.GenreID, S.SpotifyListens
+FROM Music.Song S
+ORDER BY S.SpotifyListens ASC;
+GO
